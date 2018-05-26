69436215ae7bd06d5e081c37ef0165ea1428793b
Jürgen Smolka <30924797+ttimer@users.noreply.github.com>
2018-05-26 18:29:23 +0200

    Add files via upload

---
+++ HelloJanitorWorld.java
@@ -0,0 +1,14 @@
+// JAVA - SMonitor
+// HelloJanitorWorld.java
+// http://smolka.lima-city.de
+
+public class HelloJanitorWorld {
+
+	/* @param args */
+	public static void main(String[] args) {
+		System.out.println("Hello Janitor World !");
+		System.out.println("Here is SMonitor 4 U.");
+		System.out.println(";)");
+	}
+
+}
